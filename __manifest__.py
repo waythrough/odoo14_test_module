b5134dff1ada4dcfa8dd83c77805543f096496bd
waythrough <155105630+matrizDePixeles@users.noreply.github.com>
2025-06-22 21:25:41 -0400

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "test",
+    'name': "Modulo de prueba para Odoo 14",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -10,7 +10,7 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
+    'author': "Ezequiel",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
